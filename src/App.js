5572947cc464c3c83a74316d762e07df32388529
saintsaurabh <surajsaurabh@ymail.com>
2024-04-04 22:27:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Dudy Kart build on React Really.
+          Welcome to Dudy Kart developed on React.
         </p>
         <a
           className="App-link"
